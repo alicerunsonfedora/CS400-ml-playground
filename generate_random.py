0ca251aaced1c9c25b46ae929fe5e2d1932d88d6
Marquis Kurt <software@marquiskurt.net>
2020-11-10 15:12:24 -0500

    :sparkles: [CS400-ml-playground] Add random generation

---
+++ generate_random.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+#
+# generate_random.py
+# Yellow Converse
+#
+# Created by Marquis Kurt on 11/10/20.
+# Copyright © 2020 Marquis Kurt. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
+from sys import argv
+from json import dumps
+from argparse import ArgumentParser
+from random import randint
+
+ATTRIBUTES = [
+    "canEscape",
+    "nearExit",
+    "nearInput",
+    "inputActive",
+    "inputRelevant",
+    "requiresObject",
+    "requiresCostume",
+    "wearingCostume",
+    "hasObject",
+    "nearObject",
+    "allInputsActive"
+]
+
+ACTIONS = [
+    "MOVE_LEFT",
+    "MOVE_RIGHT",
+    "MOVE_UP",
+    "MOVE_DOWN",
+    "STOP",
+    "NEXT_COSTUME",
+    "PREV_COSTUME",
+    "PICK_UP",
+    "DROP",
+    "DEPLOY",
+    "RETRACT",
+    "ACTIVATE",
+    "MOVE_RANDOM",
+    "MOVE_EXIT_CLOSER",
+    "MOVE_INPUT_CLOSER",
+    "MOVE_OBJ_CLOSER"
+]
+
+def random_bool() -> bool:
+    """Returns a random boolean value."""
+    return bool(randint(0, 1))
+
+def arguments() -> ArgumentParser:
+    """Returns an argument parser."""
+    parser = ArgumentParser(
+        "Random State Generator",
+        usage="generate_random [-h] [--amount [AMOUNT]]",
+        description="Generate random abstract states for Yellow Converse."
+    )
+    parser.add_argument("--amount", nargs='?', default=50, help="The number of states to make.")
+    return parser
+
+def make_state() -> dict:
+    """Returns a randomly-generated state dictionary with a corresponding action."""
+    state = { }
+    for attribute in ATTRIBUTES:
+        state[attribute] = random_bool()
+    state["action"] = ACTIONS[randint(0, len(ACTIONS) - 1)]
+    return state
+
+if __name__ == "__main__":
+    OPTS = arguments().parse_args()
+
+    states = []
+    for _ in range(OPTS.amount):
+        states.append(make_state())
+
+    with open("random.json", "w+") as data:
+        data.write(dumps(states, indent=4))
